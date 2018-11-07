d6baff1b33f8234c4c2d3714d7dd3947b3a82aeb
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2018-11-07 12:52:06 -0300

    Linkando as telas a partir das imagens. Faltando formatar as imagens e decidir as cores do app

--- Pibiti/app/src/main/java/com/example/mattheusbrito/pibiti/MainActivity.java
+++ Pibiti/app/src/main/java/com/example/mattheusbrito/pibiti/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.mattheusbrito.pibiti;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
+
+    private ImageView tableButton;
+    private ImageView formButton;
+    private ImageView graphicButton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tableButton = (ImageView) findViewById(R.id.tableViewID);
+        formButton = (ImageView) findViewById(R.id.formID);
+        graphicButton = (ImageView) findViewById(R.id.graphicViewID);
+
+
+        tableButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, TableActivity.class));
+            }
+        });
+
+        formButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, FormActivity.class));
+            }
+        });
+
+        graphicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GraphicActivity.class));
+            }
+        });
     }
 }

c49d32fe425b833f52719af8952ca563e8a3b87d
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2018-11-07 03:25:25 -0300

    Iniciando e vamo que vamo

---
+++ Pibiti/app/src/main/java/com/example/mattheusbrito/pibiti/FormActivity.java
@@ -0,0 +1,14 @@
+package com.example.mattheusbrito.pibiti;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+    }
+}

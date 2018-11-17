b88bb0af78349cee00da8f6349a4ded91d1b7001
MattheusB <mattheus.rodrigues@ccc.ufcg.edu.br>
2018-11-17 12:22:49 -0300

    Tela de login linkada com o main do app, falta fazer a tableActicity

--- Pibiti/app/src/main/java/com/example/mattheusbrito/pibiti/LoginActivity.java
+++ Pibiti/app/src/main/java/com/example/mattheusbrito/pibiti/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.example.mattheusbrito.pibiti;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.loginButtonID);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+
     }
 }
